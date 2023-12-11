18126ea27b46328813f642e61f4be9f67f8d16f1
thesilentgeek <111566724+thesilentgeek@users.noreply.github.com>
2023-12-11 17:11:32 +0100

    Add files via upload

---
+++ CSC314 MID SEMESTER.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+int main()
+{
+    std::string enter a word;
+    std::cin >> enter a word;
+    if( equal(s.begin(), s.begin() + s.size()/2, s.rbegin()) )
+        std::cout << "is a palindrome.\n";
+    else
+        std::cout << "is NOT a palindrome.\n";
+}
---
+++ Question 3 solution.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    // Empty stack
+    stack<int> mystack;
+    mystack.push(85);
+    mystack.push(34);
+    mystack.push(99);
+
+    // Printing content of stack
+    while (!mystack.empty()) {
+        cout << ' ' << mystack.top();
+        mystack.pop();
+    }
+}
